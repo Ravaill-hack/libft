d583c4b7d253808859abe47a51ee28b825763d8a
lmatkows <lmatkows@student.42perpignan.fr>
2024-11-07 17:05:59 +0100

    tests bonus

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:32:32 by lmatkows          #+#    #+#             */
-/*   Updated: 2024/11/07 09:20:11 by lmatkows         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:31:16 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,31 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del((lst)->content);
 	free (lst);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+void	del_test(void *element)
+{
+	free(element);
+}
+
+int	main(void)
+{
+	t_list	**lst;
+	t_list	*lst01;
+	t_list	*lst02;
+	t_list	*lst03;
+
+	lst03 = (ft_lstnew("Test 3"));
+	lst = &lst03;
+	lst02 = (ft_lstnew("Test 2"));
+	ft_lstadd_front(lst, lst02);
+	lst01 = (ft_lstnew("Test 1"));
+	ft_lstadd_front(lst, lst01);
+	puts(lst03->content);
+	ft_lstdelone(lst03, del_test);
+	puts(lst03->content);
+}
+*/
