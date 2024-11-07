b220b9d09d50cc8511bc459c9ecf207ac8f4d801
lmatkows <lmatkows@student.42perpignan.fr>
2024-11-07 18:07:37 +0100

    ft_lstiter repris une fois de plus plus plus

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:15:28 by lmatkows          #+#    #+#             */
-/*   Updated: 2024/11/07 18:03:05 by lmatkows         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:06:26 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst || !!f)
 		return ;
-	f(lst->content);
-	if (lst->next)
-		ft_lstiter(lst->next, f);
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
 }
